fc050749729e5028ebc1b2391dd3de43c830d2b8
pipjivko <pipjivko@gmail.com>
2024-05-15 22:18:14 +0300

    connection as $conn

--- forms/install_form.php
+++ forms/install_form.php
@@ -1,7 +1,7 @@
 
     
 <link rel="stylesheet" href="../css/sing_in.css"/>
-<form action="action" class="form">
+<form action="../install.php" class="form">
     <label>Insert Database Name</label>
     <input type="text" name="database_name" class="input" />
     <button type="submit"   class="submit">create_database</button>
--- install.php
+++ install.php
@@ -1,2 +1,20 @@
 <?php
 
+//var_dump($_GET);
+
+$conn =  new mysqli('localhost','root','');
+if($conn->connect_error){
+    var_dump($conn);
+}else{
+    echo "you connected";
+}
+if(isset($_GET['database_name']) && !empty($_GET['database_name'])){
+    
+$database_name = $_GET['database_name'];
+
+$query = "create database $database_name";
+$conn->query($query);
+echo "<a href='./forms/install_form.php'>install form</a>";
+        
+}
+ var_dump($query);
