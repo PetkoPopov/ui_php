696af348bfee654af19658762c5c3c10692f4d7d
pipjivko <pipjivko@gmail.com>
2024-05-16 19:38:20 +0300

    fix create table

--- install.php
+++ install.php
@@ -2,17 +2,21 @@
 
 //var_dump($_GET);
 
-$conn =  new mysqli('localhost','root','');
+if(isset($_GET['database_name']) && !empty($_GET['database_name'])){
+    
+$database_name = $_GET['database_name'];
+
+$conn =  new mysqli('localhost','root','',"$database_name");
 if($conn->connect_error){
     var_dump($conn);
 }else{
     echo "you connected";
 }
-if(isset($_GET['database_name']) && !empty($_GET['database_name'])){
-    
-$database_name = $_GET['database_name'];
-
-$query = "create database $database_name";
+//$query = "create table '$database_name'.'users' "
+//        . "('id' int null auto_increment , 'name' varchar(100) ,primary_key('id') )";
+$query = "CREATE TABLE `$database_name`.`users` (`id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(100) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+echo "<br/>";
+die($query);
 $conn->query($query);
 echo "<a href='./forms/install_form.php'>install form</a>";
         
