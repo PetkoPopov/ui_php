b43ca3c36a8467b785e6c31ddf83921ce4d32e73
pipjivko <pipjivko@gmail.com>
2024-05-17 20:12:19 +0300

    test exists database

--- install.php
+++ install.php
@@ -5,20 +5,36 @@
 if(isset($_GET['database_name']) && !empty($_GET['database_name'])){
     
 $database_name = $_GET['database_name'];
+$conn = new  mysqli('localhost','root','');
+$arr = $conn->query("use `hell`");
+setcookie("name",'petko');
+setcookie('age', 44);
+setcookie('car', 'audi');
+setcookie('mphone', 'xiomi');
 
-$conn =  new mysqli('localhost','root','',"$database_name");
+
+
+
+var_dump($_COOKIE,$_SERVER);die;
+//$conn =  new mysqli('localhost','root','',"$database_name");
 if($conn->connect_error){
     var_dump($conn);
 }else{
     echo "you connected";
 }
-//$query = "create table '$database_name'.'users' "
-//        . "('id' int null auto_increment , 'name' varchar(100) ,primary_key('id') )";
-$query = "CREATE TABLE `$database_name`.`users` (`id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(100) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+$query = "create table if not exists`$database_name`.`users` "
+        . "(`id` int not null auto_increment , `name` varchar(100) , primary key(`id`) )";
+//$query = "CREATE TABLE `$database_name`.`users` (`id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(100) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
 echo "<br/>";
-die($query);
-$conn->query($query);
+//die($query);
+die;
+if($conn->query($query)===true){
+    echo "you created table users";
+}else{
+    echo "something go wrong";
+}
+//$conn->query($query);
 echo "<a href='./forms/install_form.php'>install form</a>";
         
 }
- var_dump($query);
+// var_dump($query);
