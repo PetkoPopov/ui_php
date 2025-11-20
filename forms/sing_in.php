6bfa27299d0e6b1436fa9de17c4206667cdad76a
PetkoPopov <poetpoet2014@mail.ru>
2025-11-20 18:27:42 +0200

    create token

--- forms/sing_in.php
+++ forms/sing_in.php
@@ -2,8 +2,8 @@
 <link rel="stylesheet" href="../css/sing_in.css" />
 <form class="form" method="post" action=".././proccess/sing_in.php"> 
     <input placeholder="Enter your name" class="input" type="text" value="hello"name="f_name">
-    <input placeholder="Enter your email" class="input" type="text"value="ppp@gmail.com"name="email">
-    <input placeholder="password" class="input" type="password"value="12345" name="password">
+    <input placeholder="Enter your email" class="input" type="text"value="pipjivko@gmail.com"name="email">
+    <input placeholder="password" class="input" type="password"value="123" name="password">
     <input type="hidden" name="db_name" value="test123">
     <input type="submit" value="submit" class="button"/>
   <!--<button >Submit</button>-->
--- proccess/sing_in.php
+++ proccess/sing_in.php
@@ -1,17 +1,26 @@
 <?php
+
 session_start();
-if(isset($_POST['db_name'])){
-    
-$db_name = $_POST['db_name'];
+if (isset($_POST['db_name'])) {
+
+    $db_name = $_POST['db_name'];
 }
+$email = $_POST['email'];
+$password = $_POST['password'];
+$password = sha1($password);
 $db_name = 'test123';
 $msql = new mysqli('localhost', 'root', '', $db_name);
 $query = "select * from `users` ";
 $result = $msql->query($query);
 $r = $result->fetch_all(MYSQLI_ASSOC);
-echo '`<pre>';
-print_r($r);
-echo '`</pre>';
 
+foreach ($r as $key => $userArr) {
 
+    if ($email == $userArr['email'] && $password == $userArr['password']) {
+        echo "you in succcessfuly :)";
+        
+        $_SESSION['token'] = time();
+        echo $_SESSION['token'];
+    }
+}
 ?>
